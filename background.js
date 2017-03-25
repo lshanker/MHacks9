52d02a9082a4adff72073e3314b97532e287344e
lshanker <lshanke@purdue.edu>
2017-03-25 05:42:05 -0400

    adds code for passing messages between the background script and the content script

--- background.js
+++ background.js
@@ -1,3 +1,23 @@
 chrome.runtime.onInstalled.addListener(function(){
   console.log("Background page log");
-})
+
+  chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "hello from a content script!:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello from the content side")
+      sendResponse({farewell: "goodbye from the background side"});
+  });
+  });
+/*
+  //Send a one time message to the content page and handle the response
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response){
+      consolse.log(response.farewell);
+    });
+  });
+});
+
+
+*/
--- content.js
+++ content.js
@@ -1,2 +1,6 @@
-alert("On twitter!");
 console.log("Content page log");
+
+//Send a one time message to the background page
+chrome.runtime.sendMessage({greeting: "hello from the content side"}, function(response){
+  console.log(response.farewell);
+})
