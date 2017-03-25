a0d59f04ea776849bf7af2a1172a2e7822f35f9f
lshanker <lshanke@purdue.edu>
2017-03-25 18:55:24 -0400

    works when you scroll down the page

--- content.js
+++ content.js
@@ -1,4 +1,28 @@
 
+function get_nonce(){
+  return document.getElementById("ttft_boot_data").getAttribute("nonce");
+}
+
+
+
+
+
+var orig_nonce = get_nonce();
+var cur_nonce = get_nonce();
+
+setInterval(function(){
+  cur_nonce = get_nonce();
+  console.log(cur_nonce);
+  if(orig_nonce==cur_nonce){
+    orig_nonce = cur_nonce;
+    var el = document.getElementsByClassName( 'Tombstone' );
+    for(i =0; i<el.length; i++){
+        el[i].parentNode.parentNode.style.display = 'none';
+    }
+  }
+}, 500);
+
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
@@ -7,8 +31,8 @@ chrome.runtime.onMessage.addListener(
                 console.log("Content Has Recieved Message " + request.info)
     if (request.info == "urlChange"){
       var el = document.getElementsByClassName( 'Tombstone' );
-        for(i =0; i<el.length; i++){
+      for(i =0; i<el.length; i++){
           el[i].parentNode.style.display = 'none';
-        }
+      }
     }
   });
