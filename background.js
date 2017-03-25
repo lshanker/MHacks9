7a3835c2480336f53af9ca5f2065d6c67b74c849
lshanker <lshanke@purdue.edu>
2017-03-25 17:15:01 -0400

    Adds ability to turn extension on or off

--- background.js
+++ background.js
@@ -1,8 +1,39 @@
+
+
+var on = true;
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     console.log("TAB CHANGE");
+    if(on){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {info : "urlChange"}, function(response) {
     console.log("Content Responded");
   });
 });
+}
+});
+
+var enable=false;
+chrome.browserAction.onClicked.addListener(function (tab) {
+ enable = enable ? false : true;
+ if(enable){
+  //turn on...
+  chrome.browserAction.setIcon({ path: 'icon.png' });
+  chrome.browserAction.setBadgeText({ text: 'ON' });
+  on = true;
+  chrome.tabs.getSelected(null, function(tab) {
+  var code = 'window.location.reload();';
+  chrome.tabs.executeScript(tab.id, {code: code});
+});
+
+ }else{
+  //turn off...
+  chrome.browserAction.setIcon({ path: 'disable.png'});
+    chrome.browserAction.setBadgeText({ text: 'OFF' });
+    on = false;
+    chrome.tabs.getSelected(null, function(tab) {
+  var code = 'window.location.reload();';
+  chrome.tabs.executeScript(tab.id, {code: code});
+});
+ }
 });
